5f151f1a1ab79c3684686ed2d72dace08f617f8b
Lafaiete Santos <lafaiete21@icloud.com>
2024-05-07 16:14:42 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import LikeButton from "./components/LikeButton";
+import Counter from "./components/Counter";
+import ClickablePicture from "./components/ClickablePicture";
+import Dice from "./components/Dice";
 import "./App.css";
 
 function App() {
   return (
+    <>
     <div className="App">
       <h1> LAB | React Training</h1>
     </div>
-  );
+    <div>
+      <LikeButton />
+      <LikeButton />
+    </div>
+    <Counter />
+    <ClickablePicture />
+    <Dice />
+  </>
+);
 }
 
 export default App;
